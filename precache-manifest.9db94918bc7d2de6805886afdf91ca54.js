671c70dab305406b1e4969aaf81d9978d6ebb92c
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2018-12-11 16:05:00 +0200

    Updates

---
+++ precache-manifest.9db94918bc7d2de6805886afdf91ca54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "353862b8fd2b7727a426",
+    "url": "/fhir-check-in/static/js/runtime~main.353862b8.js"
+  },
+  {
+    "revision": "703f960405ce1012c2db",
+    "url": "/fhir-check-in/static/js/main.703f9604.chunk.js"
+  },
+  {
+    "revision": "f26da8e0765c7beb653c",
+    "url": "/fhir-check-in/static/js/1.f26da8e0.chunk.js"
+  },
+  {
+    "revision": "703f960405ce1012c2db",
+    "url": "/fhir-check-in/static/css/main.19fa031b.chunk.css"
+  },
+  {
+    "revision": "f26da8e0765c7beb653c",
+    "url": "/fhir-check-in/static/css/1.cffa9213.chunk.css"
+  },
+  {
+    "revision": "eb60ea70243bde57d409ad2e277d8253",
+    "url": "/fhir-check-in/index.html"
+  }
+];
