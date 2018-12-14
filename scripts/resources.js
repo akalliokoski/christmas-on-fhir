6eb5a3ae5163e8054e13e3a917bad77b71620db5
Antti Kalliokoski <antti.a.kalliokoski@gmail.com>
2018-12-14 14:15:33 +0200

    Update appointment participant

--- scripts/resources.js
+++ scripts/resources.js
@@ -14,15 +14,15 @@ const FHIR_BASE = "http://hapi.fhir.org/baseDstu3";
 
 const RESOURCE_IDS_FILE = path.resolve("./src/assets/fhir-resources.json");
 
-const readTemplate = async type => {
+async function readTemplate(type) {
   const filepath = path.resolve(TEMPLATES_DIR, `${type}.json`);
   return jsonfile.readFile(filepath).catch(() => {
     console.log(`Template not found for resource ${type}`);
     return null;
   });
-};
+}
 
-const getResourceUrl = (type, id = null, { jsonFormat = false } = {}) => {
+function getResourceUrl(type, id = null, { jsonFormat = false } = {}) {
   let url = `${FHIR_BASE}/${type}`;
 
   if (id !== null) {
@@ -34,19 +34,25 @@ const getResourceUrl = (type, id = null, { jsonFormat = false } = {}) => {
   }
 
   return url;
-};
+}
 
-const fetchResourceFromServer = async (type, id) => {
+async function fetchResourceFromServer(type, id) {
   return axios.get(getResourceUrl(type, id));
-};
+}
 
-const createResourceOnServer = async resource => {
+async function createResourceOnServer(resource) {
   return await axios
     .post(getResourceUrl(resource.resourceType), resource)
     .then(response => response.data);
-};
+}
 
-const getResource = async (type, id) => {
+async function updateResourceOnServer(resource) {
+  return await axios
+    .put(getResourceUrl(resource.resourceType, resource.id), resource)
+    .then(response => response.data);
+}
+
+async function getResource(type, id) {
   if (id === null || id === undefined) {
     return null;
   }
@@ -54,35 +60,84 @@ const getResource = async (type, id) => {
   return fetchResourceFromServer(type, id)
     .then(response => response.data)
     .catch(() => null);
-};
+}
 
-const createResource = async type => {
+async function createResource(type) {
   const template = (await readTemplate(type)) || {};
   const created = await createResourceOnServer(template);
   return fetchResourceFromServer(type, created.id).then(
     response => response.data
   );
-};
+}
 
-const getOrCreateResource = async (type, resourceIds) => {
+async function getOrCreateResource(type, resourceIds) {
   return (
     (await getResource(type, resourceIds[type])) || (await createResource(type))
   );
-};
+}
 
-const getPreviousResourceIds = async () => {
+async function getPreviousResourceIds() {
   return (await jsonfile.readFile(RESOURCE_IDS_FILE).catch(() => {})) || {};
-};
+}
 
-const writeResourceIds = async resources => {
+async function writeResourceIds(resources) {
   const resourceIds = resources.reduce((acc, { resourceType, id }) => {
     acc[resourceType] = id;
     return acc;
   }, {});
   jsonfile.writeFile(RESOURCE_IDS_FILE, resourceIds);
-};
+}
 
-const main = async () => {
+function generateDisplay(data, properties) {
+  const parts = properties.map(prop => data[prop]).filter(d => !!d);
+  return parts.join(", ");
+}
+
+function getDisplayName(name = []) {
+  const displayName = name.find(n => n.use === "official") || name[0];
+  return generateDisplay(displayName, ["prefix", "family", "given", "suffix"]);
+}
+
+function getDisplay(resource) {
+  switch (resource.resourceType) {
+    case RESOURCE_TYPES.Patient:
+    case RESOURCE_TYPES.Practitioner:
+      return getDisplayName(resource.name);
+    case RESOURCE_TYPES.Location:
+      return generateDisplay(resource, ["name", "description"]);
+    default:
+      return "";
+  }
+}
+
+async function updateAppointmentParticipant(resources) {
+  const typeToResource = resources.reduce((acc, resource) => {
+    acc[resource.resourceType] = resource;
+    return acc;
+  }, {});
+
+  const participant = [
+    typeToResource[RESOURCE_TYPES.Patient],
+    typeToResource[RESOURCE_TYPES.Practitioner],
+    typeToResource[RESOURCE_TYPES.Location]
+  ];
+
+  const references = participant.map(p => ({
+    actor: {
+      reference: `${p.resourceType}/${p.id}`,
+      display: getDisplay(p)
+    },
+    required: "required",
+    status: "accepted"
+  }));
+
+  const appointment = typeToResource[RESOURCE_TYPES.Appointment];
+  appointment.participant = references;
+
+  return updateResourceOnServer(appointment);
+}
+
+async function main() {
   const resourceIds = await getPreviousResourceIds();
   const resourceTypes = Object.values(RESOURCE_TYPES);
   const resources = await Promise.all(
@@ -91,6 +146,7 @@ const main = async () => {
     )
   );
 
+  await updateAppointmentParticipant(resources);
   writeResourceIds(resources);
 
   resources.forEach(resource =>
@@ -101,6 +157,6 @@ const main = async () => {
       )}`
     )
   );
-};
+}
 
 main();
